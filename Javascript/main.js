2b42f8bee79ffce88e5710d84af6666796644111
Ty Mondragon <tymondragon@gmail.com>
2018-05-04 14:38:56 -0600

    added font link to readMe to see if it works

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,17 +1,40 @@
 $(document).ready(function() {
   $("button").click(function() {
-    // console.log("button clicked");
-    // $.ajax({url: "http://api.wunderground.com/api/41c93dff69d2241b/geolookup/conditions/q/CO/Boulder.json", success: function(result){
-    $.getJSON("https://pixabay.com/api/?key=8894637-462007c160bd1caa95ff495ca&q=yellow+flowers&image_type=photo").then(function(result) {
-      for (let i=0; i< 10; i++){
-      
-      $(".image").attr("src",result.hits[i]["largeImageURL"])
-      console.log(i)
-    }
-      // let weather = result.current_observation.weather
-      // document.querySelector("p").innerHTML = result.current_observation.weather
-      // console.log(document.querySelector("p"));
+    // $.getJSON("https://pixabay.com/api/?key=8894637-462007c160bd1caa95ff495ca&q=arcade+games&image_type=photo").then(function(result) {console.log(result.hits[5]["largeImageURL"])
+    $.getJSON("https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/dfgkfivjrhcksyymh9vw.jpg").then(function(result) {
       console.log(result)
+      // for (let i = 0; i < 15; i++) {
+      //   let image = $("<img>", {
+      //     "class": "image",
+      //     "src": result.hits[i]["largeImageURL"]
+      //   })
+      //   $("#div1").append(image)
+      // }
+      // $('.image').click(function() {
+      //   alert("hello")
+      //   event.preventDefault()
+      })
+      // $(".image").attr("src",result.hits[i]["largeImageURL"])
+      // $('<div />', {
+      //       "class": 'test',
+      //       text: "a div",
+      //       click: function(e){
+      //           e.preventDefault();
+      //           alert("test")
+      //       }})
+      // $("#burger").click(function() {
+      //   let itemPrice = menu["burger"]
+      //   event.preventDefault();
+      //   let row = $("<tr></tr>")
+      //   let burg = $("<td id='royale'>Royale With Cheese</td>")
+      //   let burgPrice = $("<td id='burgPrice' class='right-align'>$8.99</td>")
+      //   $("#order").append(row, burg, burgPrice)
+      //   subNumber += itemPrice
+      //   return total(subNumber)
+
     })
+    // let weather = result.current_observation.weather
+    // document.querySelector("p").innerHTML = result.current_observation.weather
+    // console.log(document.querySelector("p"));
+    // console.log(result)
   })
-})
---
+++ readMe.md
@@ -0,0 +1,1 @@
+[I'm an inline-style link](https://fonts.google.com/?selection.family=Monoton|Press+Start+2P&query=monoto)
