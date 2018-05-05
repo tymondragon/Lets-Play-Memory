8730b3881d8d07256f6bd9ca47ef167b7dd48d84
Ty Mondragon <tymondragon@gmail.com>
2018-05-05 15:13:28 -0600

    able to randomize image selection however need to rendomize page number

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,40 +1,19 @@
 $(document).ready(function() {
   $("button").click(function() {
-    // $.getJSON("https://pixabay.com/api/?key=8894637-462007c160bd1caa95ff495ca&q=arcade+games&image_type=photo").then(function(result) {console.log(result.hits[5]["largeImageURL"])
-    $.getJSON("https://images.igdb.com/igdb/image/upload/t_screenshot_med_2x/dfgkfivjrhcksyymh9vw.jpg").then(function(result) {
+    let request="http://www.omdbapi.com/?apikey=9537e44&s=horror&type=movie&page=6"
+    $.getJSON(request).then(function(result) {
       console.log(result)
-      // for (let i = 0; i < 15; i++) {
-      //   let image = $("<img>", {
-      //     "class": "image",
-      //     "src": result.hits[i]["largeImageURL"]
-      //   })
-      //   $("#div1").append(image)
-      // }
-      // $('.image').click(function() {
-      //   alert("hello")
-      //   event.preventDefault()
+      for (let i = 0; i < 10; i++) {
+        let image = $("<img>", {
+          "class": "image",
+          "src": result.Search[[Math.floor(Math.random()*10)]].Poster
+        })
+        $("#div1").append(image)
+      }
+      $('.image').click(function() {
+        alert("hello")
+        event.preventDefault()
       })
-      // $(".image").attr("src",result.hits[i]["largeImageURL"])
-      // $('<div />', {
-      //       "class": 'test',
-      //       text: "a div",
-      //       click: function(e){
-      //           e.preventDefault();
-      //           alert("test")
-      //       }})
-      // $("#burger").click(function() {
-      //   let itemPrice = menu["burger"]
-      //   event.preventDefault();
-      //   let row = $("<tr></tr>")
-      //   let burg = $("<td id='royale'>Royale With Cheese</td>")
-      //   let burgPrice = $("<td id='burgPrice' class='right-align'>$8.99</td>")
-      //   $("#order").append(row, burg, burgPrice)
-      //   subNumber += itemPrice
-      //   return total(subNumber)
-
     })
-    // let weather = result.current_observation.weather
-    // document.querySelector("p").innerHTML = result.current_observation.weather
-    // console.log(document.querySelector("p"));
-    // console.log(result)
   })
+})
